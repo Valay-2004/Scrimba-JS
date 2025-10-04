bb2072fbdda27f55976f42c9b97f38b498b2e5b2
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-04 18:15:48 +0000

    Completed 3rd Practice

--- Practices/Practice Time Part 1/script.js
+++ Practices/Practice Time Part 1/script.js
@@ -4,14 +4,31 @@ let lastName = "Bhaladhare";
 let fullName = firstName + " " + lastName;
 console.log(fullName);
 
+
 // Second Practice Problem
 let greeting = "Hi there";
 // Create a function that logs out "Hi there, ${name}!" when called
 function greets() {
-    alert(`${greeting} ${firstName}!`);
+    // alert(`${greeting} ${firstName}!`);
     console.log(`${greeting} ${firstName}!`);
 }
 greets();
 
-// Third Practice Problem
 
+// Third Practice Problem
+let myPoints = 3;
+// create two functions, add3Points() and remove1Point(), and have
+// them add/remove points to/from the myPoints variable
+function add3Points() {
+    myPoints += 3;
+}
+function remove1Point() {
+    myPoints -= 1;
+}
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+// Call the functions to that the line below log out 10
+console.log(myPoints);

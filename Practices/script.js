82f2bd03e5c5b9fafff9589166045db470e30edb
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-04 18:06:22 +0000

    Second Practice complete

--- Practices/script.js
+++ Practices/script.js
@@ -1,7 +1,15 @@
+// first Practice Problem
 let firstName = "Valay";
 let lastName = "Bhaladhare";
-
 let fullName = firstName + " " + lastName;
-
 console.log(fullName);
+
+// Second Practice Problem
+let greeting = "Hi there";
+// Create a function that logs out "Hi there, ${name}!" when called
+function greets() {
+    alert(`${greeting} ${firstName}!`);
+    console.log(`${greeting} ${firstName}!`);
+}
+greets();
 

ae42557d81d162c8da39546e31fc3cdec59e0258
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-05 11:12:25 +0000

    Added comments for the Problem

--- Practices/Practice Time Part 1/Basic Calculator/script.js
+++ Practices/Practice Time Part 1/Basic Calculator/script.js
@@ -5,6 +5,12 @@ document.getElementById("num2-el").textContent = num2;
 let sumEl = document.getElementById("sum-el");
 
 // Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 const add = () => {
   console.log("add clicked");
   sumEl.textContent = num1 + num2;

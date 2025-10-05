c396ba2ee04cb2435f4ab1aa00be08e4348fe341
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-05 11:09:59 +0000

    Now all is done and working

--- Practices/Practice Time Part 1/Basic Calculator/script.js
+++ Practices/Practice Time Part 1/Basic Calculator/script.js
@@ -6,24 +6,26 @@ let sumEl = document.getElementById("sum-el");
 
 // Create four functions: add(), subtract(), divide(), multiply()
 const add = () => {
-    console.log("add clicked");
-    
-    sumEl.textContent = num1 + num2;
-}
+  console.log("add clicked");
+  sumEl.textContent = num1 + num2;
+};
 const subtract = () => {
-    sumEl.textContent = num1 - num2;
-}
+  console.log("subtract clicked");
+  sumEl.textContent = num1 - num2;
+};
 const divide = () => {
-    try {
-        if(num2 == 0){
-            throw new Error("Cannot divide by zero.");
-        }
-        sumEl.textContent = num1/num2;
-    } catch (error) {
-        console.error("Division Error: ", error.message);
-        return "Error: " + error.message;
+  try {
+    if (num2 == 0) {
+      throw new Error("Cannot divide by zero.");
     }
-}
+    console.log("divide clicked");
+    sumEl.textContent = num1 / num2;
+  } catch (error) {
+    console.error("Division Error: ", error.message);
+    return "Error: " + error.message;
+  }
+};
 const multiply = () => {
-    sumEl.textContent = num1 * num2;
-}
+  console.log("multiply clicked");
+  sumEl.textContent = num1 * num2;
+};

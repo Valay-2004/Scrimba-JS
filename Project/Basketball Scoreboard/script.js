2dcd9c8c53ef77ac4247e638976a2cf5bf96db65
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-06 11:08:57 +0000

    Fixing some functions

--- Project/Basketball Scoreboard/script.js
+++ Project/Basketball Scoreboard/script.js
@@ -1,1 +1,15 @@
 console.log("Getting ready");
+// home buttons
+const home1 = document.getElementById("home-1");
+const home2 = document.getElementById("home-2");
+const home3 = document.getElementById("home-3");
+const homeScore = document.getElementById("home-score");
+
+// Guest buttons
+const guest1 = document.getElementById("guest-1");
+const guest2 = document.getElementById("guest-2");
+const guest3 = document.getElementById("guest-3");
+
+const addOneHome = () => {
+    homeScore.textContent = 1;
+}

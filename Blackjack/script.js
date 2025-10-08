a2f3af2942e7e744fbede82e618facca20bb41a1
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-08 08:05:18 +0000

    Added if-else conditionals

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -1,8 +1,8 @@
 // 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
-let firstCard = Math.floor(Math.random() * 9 + 2);
-let secondCard = Math.floor(Math.random() * 9 + 2);
+let firstCard = Math.floor(Math.random() * 10 + 2);
+let secondCard = Math.floor(Math.random() * 10 + 2);
 console.log(firstCard);
 console.log(secondCard);
 
@@ -10,3 +10,11 @@ console.log(secondCard);
 let sum = firstCard + secondCard;
 console.log(sum);
 
+// If-else statements
+if( sum < 21 ){
+    console.log(`Do you want to draw a new card? 🙂`)
+} else if( sum === 21){
+    console.log(`Wohooo!! You've got BlackJack! 🥇`);
+} else{
+    console.log(`You're out of the game! 😭`);
+}

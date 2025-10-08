0906f3aefb5d28dbabc496ac20b5b8633a35fcbd
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-08 07:53:51 +0000

    Did the first one will check later if correct or not

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -1,5 +1,7 @@
 // 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
-let firstCard = Math.random() * 10;
+let firstCard = Math.floor(Math.random() * 9 + 2);
+let secondCard = Math.floor(Math.random() * 9 + 2);
 console.log(firstCard);
+console.log(secondCard);

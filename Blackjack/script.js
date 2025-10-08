f9b9cec07d8474548fa2fc56b3c2c27c0c1918f2
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-08 08:15:52 +0000

    Playing if-else (for the game yik)

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -18,3 +18,24 @@ if( sum < 21 ){
 } else{
     console.log(`You're out of the game! 😭`);
 }
+
+
+// Age Check
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+let age = 22;
+// if(age < 21){
+//     console.log(`You cannot enter the club`);
+// } else{
+//     console.log(`Welcome!`)
+// }
+
+// Get a birthday card if age is 100
+// Check if the person is elegible for a birthday card from the King! (100)
+if( age < 100){
+    console.log(`Not eligible`);
+} else if(age === 100){
+    console.log(`Here is your birthday card from the King!`);
+} else {
+    console.log(`Not eligible, you have already one`);
+}

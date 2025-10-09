bbafc04902c9a2c3bf2d072dece14affae873d3a
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-09 09:59:18 +0000

    Shown text for the cards

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -5,10 +5,12 @@ let hasBlackJack = false;
 let isAlive = true;
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 console.log(sum);
 
 function startGame() {
   // If-else statements
+  cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
   sumEl.textContent = `Sum: ${sum}`;
   if (sum < 21) {
     message = `Do you want to draw a new card?`;

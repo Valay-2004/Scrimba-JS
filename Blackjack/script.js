650ec6879d2bb6abaa57efa05c0f2e97b30ae4ea
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-17 16:31:53 +0000

    Used boolean in the newCard function for stopping the New card entry

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -35,6 +35,7 @@ function renderGame() {
     message = `Do you want to draw a new card?`;
   } else if (sum === 21) {
     message = `Wohooo!! You've got BlackJack!`;
+    hasBlackJack = true;
   } else {
     message = `You're out of the game!`;
     isAlive = false;
@@ -47,9 +48,14 @@ function renderGame() {
 //Create a function newCard() that logs out
 // "Drawing a new card from the deck!"
 function newCard() {
-  let card = getRandomCard();
-  console.log(card);
-  sum += card;
-  cards.push(card);
-  renderGame();
+  // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+  if (isAlive && !hasBlackJack) {
+    let card = getRandomCard();
+    console.log(card);
+    sum += card;
+    cards.push(card);
+    renderGame();
+  } else {
+    console.log("Is either a winner or looser");
+  }
 }

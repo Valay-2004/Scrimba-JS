e40bf8d4236e6646f4eeb45e02d8bfa1ea02bac9
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-17 15:57:51 +0000

    removed cards variables will add them into single function

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -1,28 +1,26 @@
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-let firstCard = getRandom();
-let secondCard = getRandom();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
 let isAlive = true;
 console.log(sum);
 
-function getRandom(){
+function getRandomCard() {
   let random = Math.floor(Math.random() * 10 + 2);
   return random;
 }
-// Create a new function called startGame() that calls 
+// Create a new function called startGame() that calls
 // renderGame()
-function startGame(){
+function startGame() {
   renderGame();
 }
 
 function renderGame() {
   cardsEl.textContent = `Cards: `;
   // Create a for loop that renders out all the cards instead of just two
-  cards.forEach(element => {
+  cards.forEach((element) => {
     cardsEl.textContent += element + " ";
   });
   sumEl.textContent = `Sum: ${sum}`;
@@ -39,10 +37,10 @@ function renderGame() {
   console.log(message);
 }
 
-//Create a function newCard() that logs out 
+//Create a function newCard() that logs out
 // "Drawing a new card from the deck!"
-function newCard(){
-  let card = getRandom();
+function newCard() {
+  let card = getRandomCard();
   console.log(card);
   sum += card;
   cards.push(card);

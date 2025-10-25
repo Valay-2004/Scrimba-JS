4f49a1bdd6ad553259d5cb372dc4e2ae1cd17f24
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-25 10:30:25 +0000

    Task completed

--- Practices/Practice Time Part 2/EmojiFighter/script.js
+++ Practices/Practice Time Part 2/EmojiFighter/script.js
@@ -1,8 +1,32 @@
 let stageEl = document.getElementById("stage");
 let fightButton = document.getElementById("fightButton");
 
+let fighters = [
+  "🐉",
+  "🐥",
+  "🐊",
+  "💩",
+  "🦍",
+  "🐢",
+  "🐩",
+  "🦭",
+  "🦀",
+  "🐝",
+  "🤖",
+  "🐘",
+  "🐸",
+  "🕷",
+  "🐆",
+  "🦕",
+  "🦁",
+];
+
 fightButton.addEventListener("click", function () {
   // Challenge:
   // When the user clicks on the "Pick Fighters" button, pick two random
   // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+  let emojiOne = Math.floor(Math.random() * fighters.length);
+  let emojiTwo = Math.floor(Math.random() * fighters.length);
+
+  stageEl.textContent = fighters[emojiOne] + "  vs  " + fighters[emojiTwo];
 });

5f48138974b9904aea4a7f2aba60f5f7d91b225e
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-30 04:03:13 +0000

    Got the text from the input field and saved it in the array

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -2,3 +2,14 @@ const inputEl = document.getElementById("input-el");
 const saveEl = document.getElementById("save-el");
 
 let myLeads = [];
+
+// Push the value "www.awsomelead.com" to myAray when the input button is clicked
+
+saveEl.addEventListener("click", () => {
+  // Push the value from the inputEl into the myLeads array
+  // instead of hard-coded "www.awesomeleads.com" value
+  // Google -> "get value from the input field js"
+  console.log(`button clicked`);
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});

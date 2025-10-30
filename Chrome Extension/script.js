9a9a252faa1779ea18fa0de07199c405d8135be3
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-30 04:08:28 +0000

    Added forEach to log out each entry in myLeads

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -1,15 +1,22 @@
 const inputEl = document.getElementById("input-el");
 const saveEl = document.getElementById("save-el");
 
-let myLeads = [];
+let myLeads = [
+  "www.example.com",
+  "www.awesomelead.com",
+  "www.epiclead.com",
+  "www.greatlead.com",
+];
 
 // Push the value "www.awsomelead.com" to myAray when the input button is clicked
 
 saveEl.addEventListener("click", () => {
-  // Push the value from the inputEl into the myLeads array
-  // instead of hard-coded "www.awesomeleads.com" value
-  // Google -> "get value from the input field js"
   console.log(`button clicked`);
   myLeads.push(inputEl.value);
   console.log(myLeads);
+});
+
+// Log out the items in the myLeads array using a for loop
+myLeads.forEach((element) => {
+  console.log(element);
 });

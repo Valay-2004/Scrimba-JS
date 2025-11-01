baa759b8c3c480c16b495a50c21a6797e56b5892
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-01 10:37:02 +0000

    rendered the rednerLeads into the event listener

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -1,30 +1,20 @@
 const inputEl = document.getElementById("input-el");
 const saveEl = document.getElementById("save-el");
 const ulEl = document.getElementById("ul-el");
-let myLeads = [
-  "www.example.com",
-  "www.awesomelead.com",
-  "www.epiclead.com",
-  "www.greatlead.com",
-  "https://music.youtube.com",
-];
+let myLeads = [];
 
 saveEl.addEventListener("click", () => {
-  console.log(`button clicked`);
+  // console.log(`button clicked`);
   myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
   console.log(myLeads);
 });
 
-// 1. Create a variable, listItems,
-// to hold all the HTML for the list items
-// Assign it to an empty string to begin with
-let listItems = "";
-
-// Render the leads in the unordered list using ulEl.textContent
-for (let i = 0; i < myLeads.length; i++) {
-  // 2. Add the item to the listItem variable instead of the ulEl.innerHTML
-  listItems += "<li>" + myLeads[i] + "</li>";
-}
-
-// 3. Render the listItem inside the unordered list using ulEl.innerHTMl
-ulEl.innerHTML = listItems;
+const renderLeads = () => {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+};

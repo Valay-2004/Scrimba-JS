2957da3e8c52ba3c038b4a5325646c54cc2ee5b9
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-12-14 10:30:05 +0000

    Updated the script & ADDED firebase things

--- Build a Mobile App/script.js
+++ Build a Mobile App/script.js
@@ -1,30 +1,32 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-const supabaseUrl = "https://gzypamgzaugxuiayoxzz.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
-console.log(supabase);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  databaseURL:
+    "https://leads-tracker-app-7a922-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+console.log(database);
 
-let myLeads = [];
+// Challenge: Create a const called 'referenceInDB' and use the ref function to create a reference called 'leads' in the database
+const referenceInDB = ref(database, "leads");
+
+onValue(referenceInDB, function (snapshot) {
+  console.log(snapshot.val());
+});
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-const tabBtn = document.getElementById("tab-btn");
-
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
-}
-
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-});
 
 function render(leads) {
   let listItems = "";
@@ -40,15 +42,11 @@ function render(leads) {
   ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
-});
+deleteBtn.addEventListener("dblclick", function () {});
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
+  console.log(inputEl.value);
+  // Challenge: Import the 'push' function and modify the line above to push inputEl.value to the referenceInDB in the database
+  push(referenceInDB, inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  render(myLeads);
 });
